1afb6c2a5a5f10b8563e25a7152b3fc63edfcd4f
vonjane <fengjian@skccchina.com>
2018-10-12 18:17:54 +0800

    hello world

---
+++ JS/text1.js
@@ -0,0 +1,10 @@
+$(function () {
+            $(".container").css({ opacity: .8 });   //设置透明度
+            $.backstretch([
+                  "img/684252.jpg",
+                  "img/684232.jpg",
+                  "img/1186813.jpg",
+                  "img/402710.jpg",
+                  "img/684242.jpg"
+            ], { duration: 4000, fade: 2550 });
+        });
